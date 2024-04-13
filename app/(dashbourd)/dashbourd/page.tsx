261d84b271b77f568638f19a17907d2b3f6ff5a3
Karim Osman <kareemosman199@gmail.com>
2024-04-13 18:15:55 +0000

    add tasks board

--- app/(dashbourd)/dashbourd/page.tsx
+++ app/(dashbourd)/dashbourd/page.tsx
@@ -1,3 +1,42 @@
+import { TasksBoard } from "../components/board";
+
 export default function Dashboard() {
-    return <></>
+  return (
+    <>
+      <div className="relative overflow-hidden max-w-[600px] lg:max-w-none mx-5 mt-4">
+        <div className="h-full w-full rounded-[inherit]">
+          <div className="table w-full">
+            <div className="mb-4 flex items-center gap-2">
+              <a
+                className="flex h-7 items-center cursor-pointer justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary bg-muted font-medium text-primary"
+                // href="/examples/mail"
+              >
+                Overview
+              </a>
+              <a
+                className="flex h-7 items-center justify-center cursor-pointer rounded-full px-4 text-center text-sm transition-colors hover:text-primary text-muted-foreground"
+                // href="/examples/dashboard"
+              >
+                Board
+              </a>
+              <a
+                className="flex h-7 items-center justify-center rounded-full  cursor-pointer px-4 text-center text-sm transition-colors hover:text-primary text-muted-foreground"
+                // href="/examples/cards"
+              >
+                List
+              </a>
+              <a
+                className="flex h-7 items-center cursor-pointer justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary text-muted-foreground"
+                // href="/examples/tasks"
+              >
+                Calender
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <TasksBoard />
+    </>
+  );
 }
